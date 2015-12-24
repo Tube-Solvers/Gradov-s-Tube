8d66a4d99795d5006aef140fca14753952f58bb1
Alexey Abakumkin <Bobrokorm@gmail.com>
2015-12-24 23:43:41 +0300

    Fix inital conditions

--- doit.m
+++ doit.m
@@ -2,11 +2,11 @@ function doit
     R = 0.25;
     R_1 = 0.40;
     delta = 0.2;
-    tau = 1e-3;
+    tau = 1e-1;
     n_pr = 1.46;
     F_u0 = 1000;
     alpha = 0.01;
-    tau_u = 10;
+    tau_u = 0.5;
     T_p = 500;
     T_e = 293;
 
@@ -17,7 +17,7 @@ function doit
     h_x = 1/(k_x-1);
     x = (0:k_x-1)*h_x;
 
-    k_phi = 201;
+    k_phi = 101;
     h_phi = pi/(2*(k_phi-1));
     phi = ((0:k_phi-1)*h_phi)';
 
@@ -57,7 +57,7 @@ function doit
 
     function r = F_u(t)
     %    r = F_u0 .* t .* exp(-(alpha.*t).^2./tau_u);
-       r = 1000;
+        r = 1000;
     end
 
     k_t_ = [293, 1278, 1528, 1677];
@@ -66,12 +66,18 @@ function doit
 
     function g = g(z, phi, t, T)
         % sigma = 5.67e-8;
-        % r = repmat(z, length(phi), 1).*R_1;
+        % r = repmat(linspace(R, R_1, columns(z)), rows(phi), 1);
         % k_p = squeeze(ppval(pp_k, T));
         %
-        % g = F_u(t) .* R .* k_p ./ r .* exp(-k_p .* (R_1 .- r)) - 4 .* k_p .* n_pr**2 .* sigma .* T.^4;
+        % g = F_u(tau*t) .* R .* k_p ./ r .* exp(-k_p .* (R_1 .- r)) - 4 .* k_p .* n_pr**2 .* sigma .* T.^4;
+        % % g = zeros(size(T));
+        %
+        % g(1:50, :) = 0;
+
         g = zeros(size(T));
-        g(1:10, :) = -100000;
+        if t*tau <= 2*tau
+            g(25:50, 1) = -1e6;
+        end
     end
 
     function Th = step_x(t, T, Th)
@@ -83,14 +89,14 @@ function doit
         F = (C(Th).*T .- tau.*g(z, phi, t, Th)) .* U;
 
         A(:, 1) = nan;
-        D(:, 1) = 1;
+        D(:, 1) = 0;
         B(:, 1) = 1;
-        F(:, 1) = 0;
+        F(:, 1) = Th(:, 1);
 
-        A(:, end) = 1;
+        A(:, end) = 0;
         D(:, end) = nan;
         B(:, end) = 1;
-        F(:, end) = 0;
+        F(:, end) = Th(:, end);
 
         Th = solve_tridiag(A, -B, D, -F);
     end
@@ -132,25 +138,35 @@ function doit
     function plot_temperature(i, T)
         printf("Time %d\n", i)
         fig = figure('visible', 'off');
-        imagesc(linspace(0, 1, columns(T)), linspace(1, 0, rows(T)), T, [273, 350]);
+        imagesc(linspace(R, R_1, columns(T)), linspace(pi/2, 0, rows(T)), T, [273, 19000]);
         colorbar();
         print(["plots/" mat2str(t) ".png"], "-dpng")
     end
 
+    function plot_tube(T)
+        R_vec = linspace(0.25, 0.40, columns(T));
+        P_vec = linspace(0, pi/2, rows(T))';
+        X = repmat(R_vec, rows(T), 1) .* cos(repmat(P_vec, 1, columns(T)));
+        Y = repmat(R_vec, rows(T), 1) .* sin(repmat(P_vec, 1, columns(T)));
+
+        X = reshape(X, 1, []);
+        Y = reshape(Y, 1, []);
+        Z = reshape(T, 1, []);
+
+        [xx, yy] = meshgrid(linspace(0, 0.4, 100));
+        griddata(X, Y, Z, xx, yy);
+    end
 
     T = T_e * ones(k_phi, k_x);
-    % Если установсить тока крайний слой в температуру плазмы,
-    % он охлаждается до температуры среды за одну итерацию. Почему?
-
-
-
-    T(:, [1, 2]) = T_p;
-    hold on;
+    % T(:, [1, 2]) = T_p;
+    % hold on;
     for t = 1:100
         T = iterate_step(t, T, @(t, T, Th) step_x(t, T, Th));
         T = iterate_step(t, T, @(t, T, Th) step_y(t, T, Th));
         plot_temperature(t, T);
     end
+
+    % plot_tube(T)
 end
 
 function x = solve_tridiag(a, b, c, d)
