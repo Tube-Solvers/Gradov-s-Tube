ccab342483880f6a95edc764698ff5e98e036b06
Yuri Kilochek <yuri.kilochek@gmail.com>
2015-12-25 21:40:49 +0300

    Implemented gif generation and fixed plot axis.

--- doit.m
+++ doit.m
@@ -32,7 +32,6 @@ function doit
     xl = ml(x);
     xr = mr(x);
 
-    % Точно ли это z юзается дальше? ещё есть z = r/R_1
     function z = zf(x)
         z = 1 + 1/a * atan((x - 1) * y_m);
     end
@@ -55,14 +54,14 @@ function doit
         r = 2.049 + 0.563e-3*T - 0.528e5./T.^2;
     end
 
-    function r = F_u(t)
+    %function r = F_u(t)
     %    r = F_u0 .* t .* exp(-(alpha.*t).^2./tau_u);
-        r = 1000;
-    end
+    %    r = 1000;
+    %end
 
-    k_t_ = [293, 1278, 1528, 1677];
-    k_p_ = [2e-3, 5e-3, 7.8e-3, 1e-2];
-    pp_k = splinefit(k_t_, k_p_, length(k_t_), "order", 3);
+    %k_t_ = [293, 1278, 1528, 1677];
+    %k_p_ = [2e-3, 5e-3, 7.8e-3, 1e-2];
+    %pp_k = splinefit(k_t_, k_p_, length(k_t_), "order", 3);
 
     function g = g(z, phi, t, T)
         % sigma = 5.67e-8;
@@ -75,8 +74,9 @@ function doit
         % g(1:50, :) = 0;
 
         g = zeros(size(T));
-        if t <= 2*tau
-            g(25:50, 1) = -1e6;
+        if t < 3*tau
+            %g(1:20, 1) = -1e6;
+            g(49:51, 49:51) = -1e4;
         end
     end
 
@@ -135,38 +135,45 @@ function doit
         end
     end
 
-    function plot_temperature(i, T)
-        printf("Time %d\n", i/tau)
-        fig = figure('visible', 'off');
-        imagesc(linspace(R, R_1, columns(T)), linspace(pi/2, 0, rows(T)), T, [273, 19000]);
+    function plot_temperature(t, T, file, delay)
+        graphics_toolkit('gnuplot');
+        printf('Time %d\n', t/tau);
+        fflush(stdout);
+        figure('visible', 'off');
+        imagesc(linspace(R, R_1, columns(T)), linspace(0, 2*pi*R, rows(T)), T, [min(T(:)), max(T(:))]);
         colorbar();
-        print(["plots/" mat2str(t/tau) ".png"], "-dpng")
+        print('.temp.png', '-dpng');
+        [data, map] = rgb2ind(imread('.temp.png'));
+        unlink('.temp.png');
+        if t == 0
+            imwrite(data, map, file, 'gif', 'LoopCount', Inf,
+                                            'DelayTime', delay); 
+        else
+            imwrite(data, map, file, 'gif', 'WriteMode', 'append',
+                                            'DelayTime', delay);
+        end
     end
 
-    function plot_tube(T)
-        R_vec = linspace(0.25, 0.40, columns(T));
-        P_vec = linspace(0, pi/2, rows(T))';
-        X = repmat(R_vec, rows(T), 1) .* cos(repmat(P_vec, 1, columns(T)));
-        Y = repmat(R_vec, rows(T), 1) .* sin(repmat(P_vec, 1, columns(T)));
+    %function plot_tube(T)
+    %    R_vec = linspace(0.25, 0.40, columns(T));
+    %    P_vec = linspace(0, pi/2, rows(T))';
+    %    X = repmat(R_vec, rows(T), 1) .* cos(repmat(P_vec, 1, columns(T)));
+    %    Y = repmat(R_vec, rows(T), 1) .* sin(repmat(P_vec, 1, columns(T)));
 
-        X = reshape(X, 1, []);
-        Y = reshape(Y, 1, []);
-        Z = reshape(T, 1, []);
+    %    X = reshape(X, 1, []);
+    %    Y = reshape(Y, 1, []);
+    %    Z = reshape(T, 1, []);
 
-        [xx, yy] = meshgrid(linspace(0, 0.4, 100));
-        griddata(X, Y, Z, xx, yy);
-    end
+    %    [xx, yy] = meshgrid(linspace(0, 0.4, 100));
+    %    griddata(X, Y, Z, xx, yy);
+    %end
 
     T = T_e * ones(k_phi, k_x);
-    % T(:, [1, 2]) = T_p;
-    % hold on;
-    for t = (1:100).*tau
+    for t = (0:99).*tau
         T = iterate_step(t, T, @(t, T, Th) step_x(t, T, Th));
         T = iterate_step(t, T, @(t, T, Th) step_y(t, T, Th));
-        plot_temperature(t, T);
+        plot_temperature(t, T, 'animation.gif', 0.1);
     end
-
-    % plot_tube(T)
 end
 
 function x = solve_tridiag(a, b, c, d)
